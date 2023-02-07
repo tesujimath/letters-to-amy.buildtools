40c1a6b9d1b0242b4f6ce1142bd44360e37d5ecc
Simon Guest <simon.guest@tesujimath.org>
2023-02-07 14:39:04 +1300

    Book list

--- src/bible.rs
+++ src/bible.rs
@@ -1,26 +1,122 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::{
+    collections::HashMap,
     fmt::{self, Display, Formatter},
     str::FromStr,
 };
 
+fn normalize_book(raw: &str) -> Option<&'static str> {
+    lazy_static! {
+        static ref book_list: Vec<Vec<&'static str>> = vec![
+            vec!["Genesis", "Gen"],
+            vec!["Exodus"],
+            vec!["Leviticus"],
+            vec!["Numbers"],
+            vec!["Deuteronomy"],
+            vec!["Joshua", "Josh"],
+            vec!["Judges"],
+            vec!["Ruth"],
+            vec!["1 Samuel"],
+            vec!["2 Samuel"],
+            vec!["1 Kings"],
+            vec!["2 Kings"],
+            vec!["1 Chronicles"],
+            vec!["2 Chronicles"],
+            vec!["Ezra"],
+            vec!["Nehemiah"],
+            vec!["Esther"],
+            vec!["Job"],
+            vec!["Psalms", "Psalm"],
+            vec!["Proverbs"],
+            vec!["Ecclesiastes"],
+            vec!["Song of Solomon"],
+            vec!["Isaiah"],
+            vec!["Jeremiah", "Jer"],
+            vec!["Lamentations"],
+            vec!["Ezekiel"],
+            vec!["Daniel"],
+            vec!["Hosea"],
+            vec!["Joel"],
+            vec!["Amos"],
+            vec!["Obadiah"],
+            vec!["Jonah"],
+            vec!["Micah"],
+            vec!["Nahum"],
+            vec!["Habakkuk"],
+            vec!["Zephaniah"],
+            vec!["Haggai"],
+            vec!["Zechariah"],
+            vec!["Malachi"],
+            vec!["Matthew"],
+            vec!["Mark"],
+            vec!["Luke"],
+            vec!["John"],
+            vec!["Acts"],
+            vec!["Romans", "Rom"],
+            vec!["1 Corinthians", "1 Cor"],
+            vec!["2 Corinthians", "2 Cor"],
+            vec!["Galatians"],
+            vec!["Ephesians", "Eph"],
+            vec!["Philippians", "Phil"],
+            vec!["Colossians", "Col"],
+            vec!["1 Thessalonians", "1 Thes"],
+            vec!["2 Thessalonians", "2 Thes"],
+            vec!["1 Timothy", "1 Tim"],
+            vec!["2 Timothy", "2 Tim"],
+            vec!["Titus"],
+            vec!["Philemon"],
+            vec!["Hebrews", "Heb"],
+            vec!["James"],
+            vec!["1 Peter"],
+            vec!["2 Peter"],
+            vec!["1 John", "1 Jn"],
+            vec!["2 John"],
+            vec!["3 John"],
+            vec!["Jude"],
+            vec!["Revelation", "Rev"],
+        ];
+        static ref canonical: HashMap<&'static str, &'static str> = book_list
+            .iter()
+            .flat_map(|aliases| {
+                aliases
+                    .iter()
+                    .map(|a| (*a, aliases[0]))
+                    .collect::<Vec<(&str, &str)>>()
+            })
+            .collect();
+    }
+
+    canonical.get(raw).copied()
+}
+
 pub fn extract_bible_refs(text: &str) {
     lazy_static! {
-        static ref FULL_REFERENCE_RE: Regex =
+        // TODO a reference may be either:
+        // 1. book chapter, which we use for later context
+        // 2. book chapter:verses, which we extract
+        // 3. verse, which we extract using the stored context
+        static ref REFERENCE_RE: Regex =
             Regex::new(r"([1-3]?)\s*([[:alpha:]]+)\s*(\d+:[\d:,\s-]+)").unwrap();
     }
 
-    for cap in FULL_REFERENCE_RE.captures_iter(text) {
-        let book = if cap[1].is_empty() {
+    for cap in REFERENCE_RE.captures_iter(text) {
+        let raw_book = if cap[1].is_empty() {
             cap[2].to_string()
         } else {
             format!("{} {}", &cap[1], &cap[2])
         };
 
-        let c_and_v = ChapterAndVerses::from_str(&cap[3]);
+        match normalize_book(&raw_book) {
+            Some(book) => {
+                let c_and_v = ChapterAndVerses::from_str(&cap[3]);
 
-        println!("matched {} {:?}", book, c_and_v);
+                println!("matched {} {:?}", book, c_and_v);
+            }
+            None => {
+                println!("WARNING: unknown book {}", &raw_book);
+            }
+        }
     }
 }
 
